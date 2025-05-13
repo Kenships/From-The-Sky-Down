6d2416186bcdae08c308b2e87b87e9f5c1adfad7
Kenneth <kenships0402@gmail.com>
2025-05-13 08:54:38 -0400

    Quick bugfix for setting default weighting
    bug: weighting was being always set to 5 on load.

--- Assets/Editor/DialogueSystem/Elements/DSMultipleChoiceNode.cs
+++ Assets/Editor/DialogueSystem/Elements/DSMultipleChoiceNode.cs
@@ -65,8 +65,13 @@ namespace DialogueSystem.Elements
             choicePort.userData = userData;
             
             DSChoiceSaveData choiceData = userData as DSChoiceSaveData;
-
-            choiceData.Weighting = 5;
+            
+            //if weighting is not initialized
+            if (choiceData.Weighting == 0)
+            {
+                choiceData.Weighting = 5;
+            }
+            
 
             Button deleteChoiceButton = DSElementsUtility.CreateButton("X", () =>
             {
